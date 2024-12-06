cb40834ca87cac3423edee1eef79e15b1220d2c1
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-06 22:43:33 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <vector>
 
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
 using namespace std;
 
 int n, m;
-vector<pair<int, int> > dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int dirY[4] = {-1, 1, 0, 0};
+int dirX[4] = {0, 0, 1, -1};
 
-int dfs(int y, int x, vector<vector<int> >& map)
+int map[505][505];
+
+int dfs(int y, int x)
 {
     int count = 1; 
 
     for(int idx = 0; idx < 4; ++idx)
     {
-        int ny = y + dir[idx].first;
-        int nx = x + dir[idx].second;
+        int ny = y + dirY[idx];
+        int nx = x + dirX[idx];
 
         if(ny >= 0 && ny < n && nx >= 0 && nx < m)
         {
             if(map[ny][nx] == 1)
             {
                 map[ny][nx] = 0;
-                count += dfs(ny, nx, map);
+                count += dfs(ny, nx);
             }
         }
     }
@@ -31,9 +36,9 @@ int dfs(int y, int x, vector<vector<int> >& map)
 
 int main()
 {
+    fast;
+    
     cin >> n >> m;
-
-    vector<vector<int> > map (n, vector<int>(m, 0));
 
     for(int iy = 0; iy < n; ++iy)
     {
@@ -53,7 +58,7 @@ int main()
             {
                 count++;
                 map[iy][ix] = 0;
-                maxNum = max(maxNum, dfs(iy, ix, map));
+                maxNum = max(maxNum, dfs(iy, ix));
             }
         }
     }
