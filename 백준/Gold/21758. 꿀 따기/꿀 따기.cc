3bd3d3c431c1caeb5a785836bfc708614736b8f6
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-10 00:00:54 +0900

    [Gold V] Title: 꿀 따기, Time: 32 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> honey(100002, 0);
+    vector<int> prefixSum(100002, 0);
+
+    // 입력 및 누적 합 계산
+    for (int i = 1; i <= n; ++i) {
+        cin >> honey[i];
+        prefixSum[i] = honey[i] + prefixSum[i - 1];
+    }
+
+    int answer = 0;
+
+    for (int idx = 2; idx < n; ++idx) {
+        // 벌[1] - 벌통[idx] - 벌[n]
+        answer = max(answer, 2 * prefixSum[n] - honey[1] - honey[idx] - prefixSum[idx]);
+
+        // 벌통[1] - 벌1[idx] - 벌2[n]
+        answer = max(answer, prefixSum[n - 1] - honey[idx] + prefixSum[idx - 1]);
+
+        // 벌[1] - 벌2[idx] - 벌통[n]
+        answer = max(answer, prefixSum[idx] - honey[1] + prefixSum[n - 1] - prefixSum[idx - 1]);
+    }
+
+    cout << answer;
+
+    return 0;
+}
