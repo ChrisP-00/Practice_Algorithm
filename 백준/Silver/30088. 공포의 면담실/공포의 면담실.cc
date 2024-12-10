acfae1b3e2c3db427b2a5edec28d143be4989ba1
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-10 14:51:15 +0900

    [Silver V] Title: 공포의 면담실, Time: 76 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+
+using namespace std; 
+
+int main()
+{
+   ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    priority_queue<int, vector<int>, greater<int> > q; 
+    
+    int n;
+    cin >> n;
+
+  
+    while(n--)
+    {
+        int k; 
+        cin >> k;
+        
+        long long subSum = 0;
+        while(k--)
+        {
+            long long j;
+            cin >> j;
+            subSum += j;
+        }
+        
+        q.push(subSum);
+    }
+    
+    long long sum = 0;
+    long long ttl = 0;
+    while(!q.empty())
+    {
+        sum = sum + q.top();
+        ttl += sum;
+
+        q.pop();
+    }
+    
+    cout << ttl;
+    
+    return 0;
+}
