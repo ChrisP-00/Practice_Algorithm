15c37f032ef3ef80041022ab03d3a5b7a533e937
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-11 07:51:35 +0900

    [Silver III] Title: 블로그, Time: 24 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.cc
+++ 백준/Silver/21921. 블로그/블로그.cc
@@ -7,13 +7,12 @@ using namespace std;
 int main ()
 {
     fast;
-    
+
     int people [250001];
     
     int n, x;
     cin >> n >> x; 
     
-    int maxValue = 0;
     int sum = 0;
     for(int idx = 0; idx < n; ++idx)
     {
@@ -25,28 +24,27 @@ int main ()
         }
     }
     
+    int maxValue = sum;
     int count = 1;
-    if(n > 1)
+
+    for(int idx = x; idx < n; ++idx)
     {
-        for(int idx = x; idx <= n; ++idx)
+        sum -= people[idx - x];
+        sum += people[idx];
+
+        if(maxValue < sum)
         {
-            
-            if(maxValue < sum)
-            {
-                maxValue = sum;
-                count = 1;
-            }
-            
-            else if(maxValue == sum)
-            {
-                count++;
-            }
-            
-            sum -= people[idx - x];
-            sum += people[idx];
+            maxValue = sum;
+            count = 1;
         }
+        
+        else if(maxValue == sum)
+        {
+            count++;
+        }
+
     }
-    
+
     if(maxValue == 0)
     {
         cout << "SAD";
