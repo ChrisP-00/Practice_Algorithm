198db41330c1a9fbb153a39db6ba6dfb31ee9d12
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-10 13:52:56 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.cc
@@ -0,0 +1,33 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+    int sum = 0;
+    int maxNum = 0;
+    
+    for(int idx = 0; idx < 10; ++idx)
+    {
+        int n;
+        cin >> n;
+        
+        sum += n;
+  
+        if(abs(100 - sum) < abs(100 - maxNum))
+        {
+            maxNum = sum;
+        }
+        else if(abs(100 - sum) == abs(100 - maxNum))
+        {
+            if(sum > maxNum)
+            {
+                maxNum = sum;
+            }
+        }
+    }
+    
+    cout << maxNum;
+    
+    return 0;
+}
