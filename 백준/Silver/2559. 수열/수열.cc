b616c27fbe67dc6670eab7dfeb77668bb09269f4
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-16 11:14:05 +0900

    [Silver III] Title: 수열, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,9 +1,13 @@
 #include <iostream> 
 
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
 using namespace std;
 
 int main()
 {
+    fast;
+
     int n, k;
     cin >> n >> k;
     
