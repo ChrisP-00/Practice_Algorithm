0ca541e3b8bc8b6f1da78836233a9d01b269125a
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2024-12-17 12:26:13 +0900

    [Silver IV] Title: 바닥 장식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.cc
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    
+    vector<vector<char>> room (n + 1, vector<char>(m + 1, ' '));
+    
+    int sum = 0;
+    
+    for(int iy = 1; iy <= n; ++iy)
+    {
+        for(int ix = 1; ix <= m; ++ix)
+        {
+            cin >> room[iy][ix];
+            
+            if(room[iy][ix] == '-')
+            {
+                if(room[iy][ix - 1] == '-')
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                if(room[iy - 1][ix] == '|')
+                {
+                    continue;
+                }
+            }
+            
+            sum++;
+        }
+    }
+    
+    cout << sum;
+    
+    return 0;
+}
