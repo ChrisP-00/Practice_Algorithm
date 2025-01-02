40f703f0f2a4ea2f908d1603953a5a2d8f81196c
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-01-02 22:35:38 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -2,8 +2,7 @@
 
 using namespace std; 
 
-int costs[1000][3];
-int dp[1000][3];
+int dp[1001][4];
 
 int main()
 {
@@ -14,20 +13,15 @@ int main()
     {
         for(int color = 0; color < 3; ++color)
         {
-            cin >> costs[idx][color];
+            cin >> dp[idx][color];
         }
     }
-    
-    dp[0][0] = costs[0][0];
-    dp[0][1] = costs[0][1];
-    dp[0][2] = costs[0][2];
-    
-    
+        
     for(int idx = 1; idx < n; ++idx)
     {
-        dp[idx][0] = costs[idx][0] + min(dp[idx - 1][1], dp[idx - 1][2]);
-        dp[idx][1] = costs[idx][1] + min(dp[idx - 1][0], dp[idx - 1][2]);
-        dp[idx][2] = costs[idx][2] + min(dp[idx - 1][0], dp[idx - 1][1]);
+        dp[idx][0] += min(dp[idx - 1][1], dp[idx - 1][2]);
+        dp[idx][1] += min(dp[idx - 1][0], dp[idx - 1][2]);
+        dp[idx][2] += min(dp[idx - 1][0], dp[idx - 1][1]);
     }
     
     cout << min({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
