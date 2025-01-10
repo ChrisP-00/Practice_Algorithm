67da6dca0be27efe16ddc19127c9377daa5117f3
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-01-10 22:39:55 +0900

    [Gold V] Title: 토달기, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/1897. 토달기/토달기.cc
+++ 백준/Gold/1897. 토달기/토달기.cc
@@ -1,75 +1,58 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
-#include <string>
+#include <unordered_map>
 #include <algorithm>
+
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 
 using namespace std;
 
-// 전역 변수 선언
-string ans; // 가장 긴 단어 저장
-unordered_map<string, bool> words; // 단어 존재 여부 및 처리 여부 확인
-
-// 단어를 검사하는 함수
-void check(const string &x)
-{
-    for (size_t i = 0; i < x.size(); ++i)
-    {
-        // i번째 문자를 제거한 새로운 문자열
-        string n = x.substr(0, i) + x.substr(i + 1);
-
-        // n이 words에 존재하고 이미 처리된 단어라면
-        if (words.find(n) != words.end() && words[n])
-        {
-            words[x] = true; // 현재 단어 x를 처리 완료로 표시
-            if (x.size() > ans.size()) // 더 긴 단어 발견 시 갱신
-            {
-                ans = x;
-            }
-            return;
-        }
-    }
-}
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N;
+    fast;
+    int n;
     string word;
-    cin >> N >> word;
 
-    // 단어 목록 입력 받기
-    vector<string> inputWords(N);
-    for (int i = 0; i < N; ++i)
+    cin >> n >> word; 
+
+    vector<string> words;
+
+    while(n--)
     {
-        cin >> inputWords[i];
+        string w;
+        cin >> w;
+        words.push_back(w);
     }
 
-    // 단어를 unordered_map으로 변환
-    for (const string &w : inputWords)
+    sort(words.begin(), words.end(), [](const string& a, const string& b)
     {
-        words[w] = false; // 초기값 false
-    }
-
-    // 초기 단어 설정
-    words[word] = true;
-    ans = word;
-
-    // 단어를 길이순으로 정렬
-    sort(inputWords.begin(), inputWords.end(), [](const string &a, const string &b) {
-        return a.size() < b.size();
+        return a.size() < b.size();    
     });
 
-    // 단어 검사
-    for (const string &x : inputWords)
+    unordered_map<string, int> wMap;
+    wMap[word] = 1;
+    string maxWord = word;
+
+    for(const string& curWord : words)
     {
-        check(x);
+        for(int idx = 0; idx < curWord.size(); ++idx)
+        {
+            string shorten = curWord; 
+            shorten.erase(shorten.begin() + idx);
+
+            if(wMap.count(shorten) > 0)
+            {
+                wMap[curWord] = max(wMap[curWord], wMap[shorten] + 1);
+            
+                if(wMap[curWord] > wMap[maxWord])
+                {
+                    maxWord = curWord;
+                }
+            }
+        }  
     }
 
-    // 결과 출력
-    cout << ans << '\n';
+    cout << maxWord;
 
     return 0;
 }
