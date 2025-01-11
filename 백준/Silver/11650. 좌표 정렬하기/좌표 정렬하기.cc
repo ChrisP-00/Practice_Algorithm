b8f06d4029f101fdd51962b4dc38037a8f790e15
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-01-11 21:36:21 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -2,10 +2,13 @@
 #include <vector>
 #include <algorithm>
 
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
 using namespace std;
 
 int main()
 {
+    fast;
     int n;
     cin >> n;
 
