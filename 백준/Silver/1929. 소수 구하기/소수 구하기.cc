dace957680bb5a1ab8002269a40d8d61e6da591b
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-01-11 23:57:23 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -5,23 +5,30 @@ using namespace std;
 
 void findPrimes(int n, int m)
 {
-    vector<bool> isPrime(n + 1, true);
-    isPrime[0] = isPrime[1] = false;
+    int size = (n / 2) + 1;
+    vector<bool> isPrime(size, true);
+    isPrime[0] = false;
 
-    for(int i = 2; i * i <= n; ++i)
+    for(int i = 3; i * i <= n; i += 2)
     {
-        if(isPrime[i])
+        if(isPrime[i / 2])
         {
-            for(int j = i * i; j <= n; j += i)
+            for(int j = i * i; j <= n; j += i * 2)
             {
-                isPrime[j] = false;
+                isPrime[j / 2] = false;
             }
         }
     }
 
-    for(int i = m; i <= n; ++i)
+    if(m <= 2 && n >= 2) cout << 2 << '\n';
+    if(m % 2 == 0)
     {
-        if(isPrime[i])
+        m++;
+    }
+
+    for(int i = m; i <= n; i += 2)
+    {
+        if(isPrime[i / 2])
         {
             cout << i << '\n';
         }
