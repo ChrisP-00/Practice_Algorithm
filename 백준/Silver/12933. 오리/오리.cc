1c6c2c2f9af214d46e28edba53420eee32ea4573
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-01-22 19:48:41 +0900

    [Silver II] Title: 오리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.cc
+++ 백준/Silver/12933. 오리/오리.cc
@@ -1,76 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
-
-int quackToInt(const char& c) {
-    switch (c) {
-        case 'q': return 0;
-        case 'u': return 1;
-        case 'a': return 2;
-        case 'c': return 3;
-        case 'k': return 4;
-        default: return -1;
-    }
-}
-
-int findDuck(const string& s) {
-    vector<int> ducks;
-    int duckCount = 0;
-
-    for (const char& c : s) {
-        int curDuck = quackToInt(c);
-
-        if(curDuck == -1)
-        {
-            return -1;
-        }
-
-        bool foundDuckSound = false;
-        for (int& duck : ducks) {
-            if (duck == curDuck - 1) {
-                duck = curDuck;
-                foundDuckSound = true;
-
-                if(duck == 4)
-                {
-                    duck = -1;
-                }
-                break;
-            }
-        }
-
-        if (!foundDuckSound) {
-            if (curDuck == 0) {
-                ducks.push_back(0);
-            }
-            else
-            {
-                return -1;
-            }
-        }
-
-        duckCount = max(duckCount, static_cast<int>(ducks.size()));
-    }
-
-    for(int& duck : ducks)
-    {
-        if(duck != -1)
-        {
-            return -1;
-        }
-    }
-
-    return duckCount;
-}
 
 int main() {
     string s;
     cin >> s;
 
-    int result = findDuck(s);
-    cout << result << endl;
+    int q = 0, u = 0, a = 0, c = 0, k = 0, duckCount = 0;
+    for(char character : s)
+    {
+        switch(character)
+        {
+            case 'q' : q++; break;
+            case 'u' : u++; break;
+            case 'a' : a++; break;
+            case 'c' : c++; break;
+            case 'k' : k++; 
+                if(duckCount < q - k)
+                {
+                    duckCount = q - k;
+                }
+                break;
+            default : 
+                cout << "-1";
+                return 0;
+        }
+
+        if(u > q || a > u || c > a || k > c) 
+        {
+            cout << "-1";
+            return 0;
+        }
+    }
+    
+    cout << ((q == u && u == a && a == c && c == k) ? duckCount + 1 : -1);
 
     return 0;
 }
