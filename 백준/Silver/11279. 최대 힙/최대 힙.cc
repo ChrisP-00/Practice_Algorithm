9aab602b4869cbba7da10300fdeb4ee71b7153ee
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-01-26 21:45:14 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int n; 
+    cin >> n;
+    
+    priority_queue<int> pq;
+    
+    while(n--)
+    {
+        int input;
+        cin >> input;
+        
+        if(input != 0)
+        {
+            pq.push(input);
+        }
+        else
+        {
+            if(!pq.empty())
+            {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else
+            {
+                cout << "0\n";
+            }
+        }  
+    }
+    
+    return 0;
+}
