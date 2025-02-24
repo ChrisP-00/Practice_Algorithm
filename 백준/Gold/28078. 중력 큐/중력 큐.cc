72de1070ecc5b84218981f3012b2dd0b5caa27e1
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-02-24 13:56:36 +0900

    [Gold V] Title: 중력 큐, Time: 68 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/28078. 중력 큐/중력 큐.cc
+++ 백준/Gold/28078. 중력 큐/중력 큐.cc
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <deque>
-#include <algorithm>
+#include <string>
 #define FASTIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
 using namespace std;
-int Q, Dir, B, W;
+
+int Q, Dir = 0, B = 0, W = 0;
 deque<char> DQ;
 string S;
 char C;
@@ -15,118 +14,69 @@ void input() {
     cin >> Q;
     while (Q--) {
         cin >> S;
+
         if (S == "push") {
             cin >> C;
-            if ((Dir == 0) || (Dir == 2)) {
+            
+            if ((Dir == 0) || (Dir == 2) || (Dir == 1 && C == 'w') || (Dir == 3 && C == 'w')) {
                 DQ.push_front(C);
-                if (C == 'b') {
+                if (C == 'b') B++;
+                else if (C == 'w') W++;
+            } 
+            else if (Dir == 1 && C == 'b') {
+                if (!DQ.empty() && DQ.back() == 'w') {
+                    DQ.push_front(C);
                     B++;
                 }
-                else if (C == 'w') {
-                    W++;
-                }
             }
-            else if (Dir == 1) {
-                if (C == 'b') {
-                    if (!DQ.empty()) {
-                        if (DQ.back() == 'w') {
-                            B++;
-                            DQ.push_front(C);
-                        }
-                    }
-                }
-                else if (C == 'w') {
-                    W++;
-                    DQ.push_front(C);
-                }
-            }
-            else if (Dir == 3) {
-                if (C == 'w') {
-                    W++;
-                    DQ.push_front(C);
-                }
-            }
-        }
-        else if (S == "pop") {
-            if (!DQ.empty()) {
-                if (Dir == 1) {
-                    if (DQ.back() == 'w') {
-                        DQ.pop_back();
-                        W--;
-                        while (!DQ.empty()) {
-                            if (DQ.back() == 'b') {
-                                DQ.pop_back();
-                                B--;
-                            }
-                            else {
-                                break;
-                            }
-                        };
-                    }
-                }
-                else {
-                    if (DQ.back() == 'b') {
-                        B--;
-                    }
-                    else if (DQ.back() == 'w') {
-                        W--;
-                    }
+        } 
+
+        else if (S == "pop" && !DQ.empty()) {
+            char item = DQ.back();
+            DQ.pop_back();
+
+            if (item == 'b') B--;
+            else if (item == 'w') W--;
+
+            if (item == 'w' && Dir == 1) {
+                while (!DQ.empty() && DQ.back() == 'b') {
                     DQ.pop_back();
+                    B--;
                 }
-            }
+            } 
         }
+
         else if (S == "rotate") {
             cin >> C;
             if (C == 'l') {
-                Dir--;
-                if (Dir == -1) {
-                    Dir = 3;
+                Dir = (Dir + 3) % 4; // 왼쪽 회전 (반시계)
+            } else if (C == 'r') {
+                Dir = (Dir + 1) % 4; // 오른쪽 회전 (시계)
+            }
+
+            if (Dir == 1) { // 위쪽으로 중력 (back에서 제거)
+                while (!DQ.empty() && DQ.back() == 'b') {
+                    DQ.pop_back();
+                    B--;
+                }
+            } 
+            else if (Dir == 3) { // 아래쪽으로 중력 (front에서 제거)
+                while (!DQ.empty() && DQ.front() == 'b') {
+                    DQ.pop_front();
+                    B--;
                 }
             }
-            else if (C == 'r') {
-                Dir++;
-                if (Dir == 4) {
-                    Dir = 0;
-                }
-            }
-            if (Dir == 1) {
-                while (!DQ.empty()) {
-                    if (DQ.back() == 'b') {
-                        DQ.pop_back();
-                        B--;
-                    }
-                    else {
-                        break;
-                    }
-                };
-            }
-            else if (Dir == 3) {
-                while (!DQ.empty()) {
-                    if (DQ.front() == 'b') {
-                        DQ.erase(DQ.begin());
-                        B--;
-                    }
-                    else {
-                        break;
-                    }
-                };
-            }
-        }
+        } 
+
         else if (S == "count") {
             cin >> C;
-            if (C == 'b') {
-                cout << B << "\n";
-            }
-            else if (C == 'w') {
-                cout << W << "\n";
-            }
+            cout << (C == 'b' ? B : W) << "\n";
         }
-    };
+    }
 }
 
 int main() {
     FASTIO
     input();
-
     return 0;
 }
