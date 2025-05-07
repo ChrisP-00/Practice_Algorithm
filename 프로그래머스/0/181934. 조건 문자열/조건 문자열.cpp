f52b8b6ca98f656db623e71fb452f067e790a07e
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-05-07 13:40:22 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -4,22 +4,5 @@
 using namespace std;
 
 int solution(string ineq, string eq, int n, int m) {
-    string oper = ineq + eq;
-    
-    if(oper == "<=")
-    {
-        return n <= m;
-    }
-    else if(oper == "<!")
-    {
-        return n < m;
-    }
-    else if(oper == ">=")
-    {
-        return n >= m;
-    }
-    else
-    {
-        return n > m;
-    }
+    return eq == "=" ? (ineq == "<" ? n <= m : n >= m) : (ineq == ">" ? n > m : n < m);
 }
