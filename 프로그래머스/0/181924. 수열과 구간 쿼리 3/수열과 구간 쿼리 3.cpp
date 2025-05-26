85395206a1926c8dfdff5fb961ba69bdb5f76a73
Jisoo Park <108470058+ChrisP-00@users.noreply.github.com>
2025-05-26 10:41:52 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.21 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    
+    for(vector<int>& q : queries)
+    {
+        swap(arr[q[0]], arr[q[1]]);
+    }
+    
+    return arr;
+}
